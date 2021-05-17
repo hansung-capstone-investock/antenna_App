ae9fbb655c8d6137ced33d72cf5d23b54e0b8334
보근 유 <qhrms97@naver.com>
2021-05-17 09:39:21 +0900

    05/17 News 정보 가져오기 수정중

---
+++ app/src/main/java/com/example/antenna/dataclass/NewsData.kt
@@ -0,0 +1,8 @@
+package com.example.antenna.dataclass
+
+data class NewsData (
+    var title : String,
+    var summary : String,
+    var publishDay : String,
+    var link : String
+)
---
+++ app/src/main/java/com/example/antenna/interface/MainNews.kt
@@ -0,0 +1,19 @@
+package com.example.antenna.`interface`
+
+import com.example.antenna.dataclass.NewsData
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MainNews {
+
+    @GET("/views/api/news")
+    fun getNews(
+            @Query("title") title: String? = null,
+            @Query("summary") summary: String? = null,
+            @Query("publishDay") publishDay: String? = null,
+            @Query("link") link: String? = null
+        ) : Call<List<NewsData>>
+}
